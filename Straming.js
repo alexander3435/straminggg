15a27b9019fac7423b1015cec0398ded499d8ddd
alexander3435 <45250031+alexander3435@users.noreply.github.com>
2018-12-10 15:06:09 +0200

    Update Straming.js

--- Straming.js
+++ Straming.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 
 
  const devs = ['507700225201274913' , '' , ''];
-const adminprefix = "!";
+const adminprefix = "%";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
